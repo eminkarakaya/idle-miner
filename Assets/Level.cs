0deef0d4c6e6e99612be2ddb9c11828c6d935542
Emin Karakaya <71080980+eminkarakaya@users.noreply.github.com>
2022-09-20 19:12:41 +0300

    .
    .

---
+++ Assets/Level.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public Manager manager;
+    public int isciKapasitesi;
+    public int level;
+    public List<Miner> levelMiners;
+}
---
+++ Assets/Miner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State{
+    kazma,
+    cuvallaGitme,
+    kazmaylaGitme,
+    
+}
+public class Miner : MonoBehaviour
+{
+    public float madencilikHizi;
+
+}
